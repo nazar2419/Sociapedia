d83a0f64e562c4bbb87dcb8f311088321e8358bb
nazar2419 <nazarx095@gmail.com>
2023-05-09 15:44:07 +0300

    Authentication and Authorization in node

--- server/index.js
+++ server/index.js
@@ -8,7 +8,9 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import authRoutes from './routes/auth.js';
 import { register } from './controllers/auth.js';
+import { verifyToken } from './middleware/auth.js';
 
 /* CONFIGURaTIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -35,6 +37,9 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES   */
 app.post('/auth/register', upload.single('picture'), register);
+
+/* ROUTES */
+app.post('/auth', authRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
