b49624a141577ca5ecf1a953825bf123812672ca
nazar2419 <nazarx095@gmail.com>
2023-05-10 14:01:17 +0300

    user routes setup

--- server/index.js
+++ server/index.js
@@ -9,10 +9,10 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import authRoutes from './routes/auth.js';
+import userRoutes from './routes/users.js';
 import { register } from './controllers/auth.js';
-import { verifyToken } from './middleware/auth.js';
 
-/* CONFIGURaTIONS */
+/* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
 
-/*FILE STORaGE */
+/*FILE STORAGE */
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'public/assets');
@@ -39,7 +39,8 @@ const upload = multer({ storage });
 app.post('/auth/register', upload.single('picture'), register);
 
 /* ROUTES */
-app.post('/auth', authRoutes);
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
